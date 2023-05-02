3c6fc815bca386c5b8654cd4d0d7f553461aac58
Snatiolam <santiago.alzate1@hotmail.com>
2023-05-01 20:21:28 -0500

    Funciona con conexion a mysql

--- public/js/Controllers/BicicletaController.js
+++ public/js/Controllers/BicicletaController.js
@@ -1,7 +1,8 @@
 const Bicicleta = require("../model/Bicicleta");
 
-exports.list = function (req, res) {
+exports.list = async function (req, res) {
     // hola = Bicicleta();
+    await Bicicleta.listAll();
     res.render("bicicletas/index", { bicis: Bicicleta.allBicis });
 };
 
@@ -9,8 +10,8 @@ exports.apiList = function (req, res) {
     res.json(Bicicleta.allBicis);
 };
 
-exports.show = function (req, res) {
-    var bici = Bicicleta.findById(req.params.id);
+exports.show = async function (req, res) {
+    var bici = await Bicicleta.findById(req.params.id);
     res.render("bicicletas/show", { bici });
 };
 
@@ -18,10 +19,9 @@ exports.create_get = function (req, res) {
     res.render("bicicletas/create");
 };
 
-exports.create_post = function (req, res) {
-    var bici = new Bicicleta(req.body.id, req.body.color, req.body.modelo);
-    bici.ubicacion = [req.body.lat, req.body.lng];
-    Bicicleta.add(bici);
+exports.create_post = async function (req, res) {
+    var bici = new Bicicleta(req.body.id, req.body.color, req.body.modelo, req.body.lat, req.body.lng);
+    await Bicicleta.add(bici);
     res.redirect("/bicicletas");
 };
 
@@ -30,15 +30,14 @@ exports.update_get = function (req, res) {
     res.render("bicicletas/update", { bici });
 };
 
-exports.update_post = function (req, res) {
-    var newBici = new Bicicleta(req.body.id, req.body.color, req.body.modelo);
-    newBici.ubicacion = [req.body.lat, req.body.lng];
-    Bicicleta.update(req.params.id, newBici);
+exports.update_post = async function (req, res) {
+    var newBici = new Bicicleta(req.body.id, req.body.color, req.body.modelo, req.body.lat, req.body.lng);
+    await Bicicleta.update(req.params.id, newBici);
     res.redirect("/bicicletas");
 };
 
 
-exports.delete = function (req, res) {
-    Bicicleta.removeById(req.body.id);
+exports.delete = async function (req, res) {
+    await Bicicleta.removeById(req.body.id);
     res.redirect("/bicicletas");
 };
--- public/js/model/Bicicleta.js
+++ public/js/model/Bicicleta.js
@@ -1,8 +1,48 @@
-let Bicicleta = function (id, color, modelo, ubicacion) {
+// const redis = require('redis');
+// const redisClient = redis.createClient({
+//     socket: {
+//         host: 'some-redis',
+//         port: '6379'
+//     },
+//     username: '',
+//     password: ''
+// });
+
+// redisClient.on('error', err => console.log('Redis Server Error', err));
+
+
+// (async () => {
+//     await redisClient.connect();
+//     await redisClient.set('ky', 'value');
+//     const value = await redisClient.get('key');
+//     console.log(value);
+//     // await redisClient.disconnect();
+// })();
+
+// ----------------------------------------------------------- //
+
+var mysql = require('mysql');
+var con = mysql.createConnection({
+  host: "mysqldb",
+  user: "mysqluser",
+  password: "password123",
+  database: "redbicis"
+});
+
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("You are connected!");
+});
+// con.end();
+
+// ------------------------------------------------------------ //
+
+let Bicicleta = function (id, color, modelo, lat, lng) {
     this.id = id;
     this.color = color;
     this.modelo = modelo;
-    this.ubicacion = ubicacion;
+    this.lat = lat;
+    this.lng = lng;
 };
 
 Bicicleta.prototype.toString = function () {
@@ -11,24 +51,65 @@ Bicicleta.prototype.toString = function () {
 
 Bicicleta.allBicis = [];
 
-Bicicleta.add = function (aBici) {
+Bicicleta.listAll =  function() {
+    // console.log('Acabo de entrar a listar keys\n\n');
+    // redisClient.keys("*", function (err, keys){
+    //     console.log("Voy a empezar a iterar");
+    //     if (err) {
+    //         console.log(err);
+    //         return;
+    //     }
+    //     console.log(keys);
+    //     keys.array.forEach(element => {
+    //         console.log(element);
+    //     });
+    // });
+}
+
+Bicicleta.add = async function (aBici) {
+    try {
+        let query = 'INSERT INTO Bicicleta SET ' + con.escape(aBici);
+        con.query(query, (err, results, fields) => {
+            console.log(results);
+        });
+        // await redisClient.set(aBici.id, JSON.stringify(aBici));
+        console.log(query);
+    } catch (error) {
+        console.log(error);
+    }
     Bicicleta.allBicis.push(aBici);
 };
 
-Bicicleta.findById = function (aBiciId) {
-    var aBici = Bicicleta.allBicis.find((x) => x.id == aBiciId);
-    if (aBici) return aBici;
-    else throw new Error(`No existe una Bicicleta con el id: ${aBiciId}`);
+Bicicleta.findById = async function (aBiciId) {
+    try {
+        // var aBici = Bicicleta.allBicis.find((x) => x.id == aBiciId);
+        // var rediBici = await redisClient.get(aBiciId);
+        var rediBici = 2;
+        // console.log(rediBici);
+        // if (aBici) return aBici;
+        if (rediBici) return JSON.parse(rediBici);
+        else throw new Error(`No existe una Bicicleta con el id: ${aBiciId}`);
+    } catch (error) {
+        console.log(error);
+    }
+    
 };
 
-Bicicleta.removeById = function (aBiciId) {
-    var aBici = Bicicleta.findById(aBiciId);
-    for (let i = 0; i < Bicicleta.allBicis.length; i++) {
-        if (Bicicleta.allBicis[i].id == aBiciId) {
-            Bicicleta.allBicis.splice(i, 1);
-            break;
+Bicicleta.removeById = async function (aBiciId) {
+    try {
+        var aBici = Bicicleta.findById(aBiciId);
+        for (let i = 0; i < Bicicleta.allBicis.length; i++) {
+            if (Bicicleta.allBicis[i].id == aBiciId) {
+                Bicicleta.allBicis.splice(i, 1);
+                break;
+            }
         }
+        // await redisClient.del(aBiciId);
+    } catch (error) {
+        console.log(error);
     }
+    
+    
 };
 
 Bicicleta.update = function (id, newBici) {
