657537842271ad33c2e0a02db3307431877898c8
Snatiolam <santiago.alzate1@hotmail.com>
2023-05-05 09:07:30 -0500

    Cambio en rutas

--- crud/routes/bicicletas.js
+++ crud/routes/bicicletas.js
@@ -7,13 +7,126 @@ const CustomStrategy = require('passport-custom').Strategy;
 const session = require('express-session');
 const request = require('request');
 
-router.get("/", bicicletaController.list);
-router.get("/:id/show", bicicletaController.show);
-router.get("/create", bicicletaController.create_get);
-router.post("/create", bicicletaController.create_post);
-router.get("/:id/update", bicicletaController.update_get);
-router.post("/:id/update", bicicletaController.update_post);
-router.post("/:id/delete", bicicletaController.delete);
+router.get("/", (req, res, next) => {
+  passport.authenticate('jwt', { session: false }, (err, user, info) => {
+    if (err) {
+      console.log('error is', err);
+      res.status(500).send('An error has occurred, we cannot greet you at the moment.');
+      return;
+    }
+
+    if (!user) {
+      res.status(401).json({ message: 'No estás autorizado para acceder a este recurso.' });
+      return;
+    }
+
+    next();
+  })(req, res, next);
+ }, bicicletaController.list);
+
+
+router.get("/:id/show", (req, res, next) => {
+  passport.authenticate('jwt', { session: false }, (err, user, info) => {
+    if (err) {
+      console.log('error is', err);
+      res.status(500).send('An error has occurred, we cannot greet you at the moment.');
+      return;
+    }
+
+    if (!user) {
+      res.status(401).json({ message: 'No estás autorizado para acceder a este recurso.' });
+      return;
+    }
+
+    next();
+  })(req, res, next);
+ }, bicicletaController.show);
+
+
+router.get("/create",  (req, res, next) => {
+  passport.authenticate('jwt', { session: false }, (err, user, info) => {
+    if (err) {
+      console.log('error is', err);
+      res.status(500).send('An error has occurred, we cannot greet you at the moment.');
+      return;
+    }
+
+    if (!user) {
+      res.status(401).json({ message: 'No estás autorizado para acceder a este recurso.' });
+      return;
+    }
+
+    next();
+  })(req, res, next);
+ },bicicletaController.create_get);
+ 
+router.post("/create", (req, res, next) => {
+  passport.authenticate('jwt', { session: false }, (err, user, info) => {
+    if (err) {
+      console.log('error is', err);
+      res.status(500).send('An error has occurred, we cannot greet you at the moment.');
+      return;
+    }
+
+    if (!user) {
+      res.status(401).json({ message: 'No estás autorizado para acceder a este recurso.' });
+      return;
+    }
+
+    next();
+  })(req, res, next);
+ }, bicicletaController.create_post);
+
+router.get("/:id/update", (req, res, next) => {
+  passport.authenticate('jwt', { session: false }, (err, user, info) => {
+    if (err) {
+      console.log('error is', err);
+      res.status(500).send('An error has occurred, we cannot greet you at the moment.');
+      return;
+    }
+
+    if (!user) {
+      res.status(401).json({ message: 'No estás autorizado para acceder a este recurso.' });
+      return;
+    }
+
+    next();
+  })(req, res, next);
+ }, bicicletaController.update_get);
+
+router.post("/:id/update", (req, res, next) => {
+  passport.authenticate('jwt', { session: false }, (err, user, info) => {
+    if (err) {
+      console.log('error is', err);
+      res.status(500).send('An error has occurred, we cannot greet you at the moment.');
+      return;
+    }
+
+    if (!user) {
+      res.status(401).json({ message: 'No estás autorizado para acceder a este recurso.' });
+      return;
+    }
+
+    next();
+  })(req, res, next);
+ }, bicicletaController.update_post);
+
+router.post("/:id/delete", (req, res, next) => {
+  passport.authenticate('jwt', { session: false }, (err, user, info) => {
+    if (err) {
+      console.log('error is', err);
+      res.status(500).send('An error has occurred, we cannot greet you at the moment.');
+      return;
+    }
+
+    if (!user) {
+      res.status(401).json({ message: 'No estás autorizado para acceder a este recurso.' });
+      return;
+    }
+
+    next();
+  })(req, res, next);
+ }, bicicletaController.delete);
 
 router.get('/greetme', (req, res, next) => {
   passport.authenticate('jwt', { session: false }, (err, user, info) => {
